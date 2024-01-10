14342bf8723e9cf83d22828ab4bce98b13ad4d6a
Milk-Maven <tylerf66@gmail.com>
2024-01-10 11:25:31 -0600

    update shared

--- src/lib/shared/utils.ts
+++ src/lib/shared/utils.ts
@@ -1,7 +1,10 @@
 
 export const endpoints = {
-  offeringCreate: 'offering/create',
-  offeringGet: 'offering/get'
+  makeOffering: 'offering/make',
+  start: 'game/start',
+  end: 'game/end',
+  snapshot: 'game/snapshot',
+  pay: 'game/pay'
 }
 
 export function checkCondition(condition: boolean, errorMessage: string): void {
@@ -26,8 +29,8 @@ export type OfferingOptionsExtraDataRequest = {
 export enum PostType { offering = 'offering', startWeek = 'startWeek' }
 export type StartWeekRequest = {
   description: string, // welcome to week three of the golden calf's trial. Until x/xx/xx/ users can submit an offering at gc.com. The top 3 choosen posts will be selected on x/xx/xx. Users can then vote on the option which they think is correct. Below the golden calf will post submissions from the app that users can directly vote on through their feed.
-  isO: 'true'
-  creatorPublicKey: string
+  latestWeek: 'true'
+  currentWeek: string
   postType: PostType.startWeek
 };
 
